dfc9804247941dc63559b8e69d6b7d7d22d93099
Mansur Saleem <mansurmsaleem@gmail.com>
2021-07-12 11:46:10 -0400

    Initial signup page. CRUD still needs to be added.

--- Client-Side/src/App.js
+++ Client-Side/src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Signup from "./components/Signup"
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-      <form id="signup" name="signup" method="POST" action="/users/signup">
-      
-      </form>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+
+    <Signup />
+
+    </>
+  );
 }
+
 export default App;
--- Server-Side/routes/users.js
+++ Server-Side/routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 
-router.get('/signup', function(req, res, next) {
+router.get('/users', function(req, res, next) {
   res.render('signup');
 });
 
