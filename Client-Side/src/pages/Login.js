091378fce5cf3d8334564db984489722062e8990
Olawale Lasisi <olawale@mevron.com>
2021-07-25 19:31:01 -0500

    pages -> login.js -> Applied CSS Classes to beautify the UI

--- Client-Side/src/pages/Login.js
+++ Client-Side/src/pages/Login.js
@@ -1,55 +1,59 @@
-import React, { useState, useContext } from "react";
-import axios from "axios";
-import { useHistory } from "react-router-dom";
-import { AuthContext } from "../helpers/AuthContext";
-
+import React, { useState, useContext } from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
+import { AuthContext } from '../helpers/AuthContext';
 
 function Login() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
   const { setAuthState } = useContext(AuthContext);
 
   let history = useHistory();
 
   const login = () => {
     const data = { username: username, password: password };
-    axios.post("http://localhost:3001/auth/login", data).then((response) => {
+    axios.post('http://localhost:3001/auth/login', data).then((response) => {
       if (response.data.error) {
         alert(response.data.error);
       } else {
-        localStorage.setItem("accessToken", response.data.token);
+        localStorage.setItem('accessToken', response.data.token);
         setAuthState({
           username: response.data.username,
-          id: response.data.user_id,
+          id: response.data.id,
           status: true,
         });
-        history.push("/post");
+        history.push('/');
       }
     });
   };
   return (
-   
-    <div className="loginContainer"> 
-    
-      <label>Username:</label>
-      <input
-        type="text"
-        onChange={(event) => {
-          setUsername(event.target.value);
-        }}
-      />
-      <label>Password:</label>
-      <input
-        type="password"
-        onChange={(event) => {
-          setPassword(event.target.value);
-        }}
-      />
-      
-      <button onClick={login()}>NEW LOGIN</button>
+    <div className='max-w-md mx-auto mt-20 border py-4 px-8'>
+      <div className='loginContainer flex flex-col max-w-md justify-center align-center'>
+        <label>Username:</label>
+        <input
+          type='text'
+          className='py-2 my-2 border border rounded'
+          onChange={(event) => {
+            setUsername(event.target.value);
+          }}
+        />
+        <label>Password:</label>
+        <input
+          type='password'
+          className='py-2 my-2 border border rounded'
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
 
+        <button
+          onClick={login}
+          className='bg-blue-900 hover:bg-blue-800 py-2 rounded text-blue-50 mt-2'>
+          {' '}
+          Login{' '}
+        </button>
+      </div>
     </div>
-   
   );
 }
 
