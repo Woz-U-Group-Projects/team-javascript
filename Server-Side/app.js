c765e8ec5a26fbb603704e9056d5f0ffbc2090c3
Olawale Lasisi <olawale@mevron.com>
2021-07-31 13:06:59 -0500

    Resolved Cross Origin Site Request issue during login…

--- Server-Side/app.js
+++ Server-Side/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 var models = require('./models');
 
 var indexRouter = require('./routes/index');
@@ -20,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
