11d04abbd8091dcf3e86f57f1a2ed23b5ad27de9
Olawale Lasisi <olawale@mevron.com>
2021-07-31 11:29:03 -0500

    src/pages/Registration.js -----------> Changed API  registration url to the correct one

--- Client-Side/src/pages/Registration.js
+++ Client-Side/src/pages/Registration.js
@@ -16,7 +16,7 @@ function Registration() {
   });
 
   const onSubmit = (data) => {
-    axios.post('http://localhost:3001/auth', data).then(() => {
+    axios.post('http://localhost:3001/users', data).then(() => {
       console.log(data);
     });
   };
