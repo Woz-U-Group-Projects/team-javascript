455e6cf59fe26f8a591b1b3177cc0771ee40cb94
Mansur Saleem <mansurmsaleem@gmail.com>
2021-08-08 19:56:47 -0400

    Addition to app.css

--- Client-Side/src/http-common.js
+++ Client-Side/src/http-common.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+//3000 or 3001? We will see...
+
 export default axios.create({
   baseURL: "http://localhost:3000/api",
   headers: {
--- Client-Side/src/index.js
+++ Client-Side/src/index.js
@@ -12,7 +12,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-//Express is running on port 3001 and React runs on  port 3000
+//Express is running on port 3001 and React runs on port 3000
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
